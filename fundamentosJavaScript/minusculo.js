d6b88cdc16327370373fed43aa2be35a45e9c9ea
DanMacedo99 <109245191+DanMacedo99@users.noreply.github.com>
2022-09-20 15:19:51 -0300

    Add files via upload

---
+++ fundamentosJavaScript/minusculo.js
@@ -0,0 +1,17 @@
+
+//como tornar uma string inteira minúscula
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // resposta true
+
+
+
+
+
+
+//quantidade de caracteres em uma string
+const senha = "danilo macedo sousa pereira"
+console.log(senha.length) // 27 caracteres
